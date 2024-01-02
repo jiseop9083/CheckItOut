c0e619dec1f60bd10467993bf2b29d203b02b5cc
janukim <pistachio1118@gmail.com>
2024-01-02 17:59:27 +0900

    Feat: dialog including two recyclerviews(attend, absent) and close button

--- app/src/main/java/com/example/madcampweek1/AttendanceAdapter.kt
+++ app/src/main/java/com/example/madcampweek1/AttendanceAdapter.kt
@@ -10,17 +10,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.util.Collections
 
-
-class AttendanceAdapter(val context: Context, private val items: List<AttendanceAdapter.DialogItem>) : RecyclerView.Adapter<AttendanceAdapter.ViewHolder>() {
-
-//    lateinit var recyclerView: RecyclerView
+    class AttendanceAdapter(private val items: List<String>) : RecyclerView.Adapter<AttendanceAdapter.ViewHolder>() {
 
     data class DialogItem(var type: Int, val data: String)
     val TYPE_ATTEND = 0
     val TYPE_ABSENT = 1
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val calenderPersonView: View = itemView.findViewById(R.id.calender_person_view)
         val textView: TextView = itemView.findViewById(R.id.text_view)
     }
 
@@ -31,42 +27,14 @@ class AttendanceAdapter(val context: Context, private val items: List<Attendance
             } else {
                 throw IllegalArgumentException("Invalid view type: $viewType")
             }
-//        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.calendar_person_item, parent, false)
-//        return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position].data
-        holder.itemView.layoutParams.width = (ScreenUtils.getScreenSize(context).x * 0.5).toInt()
-
-//        val viewHolder = holder as ViewHolder
-//        viewHolder.bind(item.data)
-//
-//        holder.textView.text = data[position]
+        holder.textView.text = items[position]
     }
 
     override fun getItemCount(): Int {
         return items.size
-    }
-
-    override fun getItemViewType(position: Int): Int {
-//        return if (position % 2 == 0) TYPE_ATTEND else TYPE_ABSENT
-        return items[position].type
-    }
-
-    fun onItemMove(fromPosition: Int, toPosition: Int): Boolean {
-
-        // 아이템 위치 변경
-        Collections.swap(items, fromPosition, toPosition)
-
-        // 아이템 타입 변경 (예시: 1열과 2열을 번갈아가면서 변경)
-        items[fromPosition].type = if (toPosition % 2 == 0) TYPE_ATTEND else TYPE_ABSENT
-
-        // RecyclerView에 위치 및 타입 변경을 알림
-        notifyItemMoved(fromPosition, toPosition)
-        notifyItemChanged(toPosition)
-
-        return true
     }
 
 }
--- app/src/main/java/com/example/madcampweek1/CalendarDialogFragment.kt
+++ app/src/main/java/com/example/madcampweek1/CalendarDialogFragment.kt
@@ -1,48 +1,29 @@
 package com.example.madcampweek1
 
-import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import android.util.Log
+import android.widget.ImageButton
 import android.widget.TextView
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import java.util.*
+
 
 class CalendarDialogFragment : DialogFragment() {
 
-//    private lateinit var attendView: RecyclerView
-//    private lateinit var absentView: RecyclerView
-//    private lateinit var attendAdapter: AttendanceAdapter
-//    private lateinit var absentAdapter: AttendanceAdapter
+    private lateinit var attendView: RecyclerView
+    private lateinit var absentView: RecyclerView
+    private lateinit var attendAdapter: AttendanceAdapter
+    private lateinit var absentAdapter: AttendanceAdapter
 
-    private lateinit var attendanceView: RecyclerView
-    private lateinit var attendanceAdapter: AttendanceAdapter
-    private lateinit var mContext : Context
-
-
-
-    //// ddddddd
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is MainActivity) {
-            mContext = context
-
-        }
-    }
 
 
     override fun onCreateView(
@@ -64,73 +45,31 @@ class CalendarDialogFragment : DialogFragment() {
         val textView = view.findViewById<TextView>(R.id.clickedDate)
         textView.text = date.format(formatterOutput)
 
-
-
-
-//        attendView = view.findViewById(R.id.attendRecyclerView)
-//        absentView = view.findViewById(R.id.absentRecyclerView)
-        attendanceView = view.findViewById(R.id.attendanceRecyclerView)
-        //val layoutManager = GridLayoutManager(context, 2)
+        attendView = view.findViewById(R.id.attendRecyclerView)
+        absentView = view.findViewById(R.id.absentRecyclerView)
 
 
         // 데이터 초기화
-//        val data1 = listOf("김민지", "강해린")
-//        val data2 = listOf("다니엘", "하니", "혜인")
-        val data = listOf(
-            AttendanceAdapter.DialogItem(0, "강해린"),
-            AttendanceAdapter.DialogItem(0, "김민지"),
-            AttendanceAdapter.DialogItem(0, "신지섭"),
-            AttendanceAdapter.DialogItem(1, "다니엘"),
-            AttendanceAdapter.DialogItem(1, "하니"),
-            AttendanceAdapter.DialogItem(1, "혜인")
-        )
+        val data1 = listOf("김민지", "강해린")
+        val data2 = listOf("다니엘", "하니", "혜인")
 
         // 어댑터 초기화
-//        attendAdapter = AttendanceAdapter(data1)
-//        absentAdapter = AttendanceAdapter(data2)
-        attendanceAdapter = AttendanceAdapter(mContext, data)
+        attendAdapter = AttendanceAdapter(data1)
+        absentAdapter = AttendanceAdapter(data2)
 
         // 리사이클러뷰 설정
-//        setupRecyclerView(attendView, attendAdapter)
-//        setupRecyclerView(absentView, absentAdapter)
-        attendanceView.adapter = attendanceAdapter
-//        attendanceAdapter.recyclerView = attendanceView
+        setupRecyclerView(attendView, attendAdapter)
+        setupRecyclerView(absentView, absentAdapter)
 
-        val layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
-
-//        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-//            override fun getSpanIndex(position: Int, spanCount: Int): Int {
-//                return when (attendanceAdapter.getItemViewType(position)) {
-//                    attendanceAdapter.TYPE_ATTEND -> 0
-//                    attendanceAdapter.TYPE_ABSENT -> 1
-//                    else -> 1
-//                }
-////                return super.getSpanIndex(position, spanCount)
-//            }
-////            override fun getSpanSize(position: Int): Int {
-////                return when (attendanceAdapter.getItemViewType(position)) {
-////                    attendanceAdapter.TYPE_ATTEND -> 0 // 1열
-////                    attendanceAdapter.TYPE_ABSENT -> 1 // 2열
-////
-////                    // TODO: 사실 todo 아니고 개빡쳐서 달아놓음
-////                    else -> 1 // 기본적으로 1열
-////                }
-////            }
-//        }
-        attendanceView.layoutManager = layoutManager
-
-        // ItemTouchHelper 설정
-//        val itemTouchHelper = ItemTouchHelper(MyItemTouchHelperCallback(attendAdapter, absentAdapter))
-//        itemTouchHelper.attachToRecyclerView(attendView)
-//        itemTouchHelper.attachToRecyclerView(absentView)
-        val itemTouchHelper = ItemTouchHelper(MyItemTouchHelperCallback(attendanceAdapter))
-        itemTouchHelper.attachToRecyclerView(attendanceView)
-
-
+        val closeButton = view.findViewById<ImageButton>(R.id.closeButton)
+        Log.d("DialogFragment", "closeButton: $closeButton")
+        closeButton.setOnClickListener {
+            // 여기에 'X' 버튼 클릭 시 수행할 동작을 추가
+            dismiss() // 다이얼로그 닫기
+        }
 
         return view
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_calendar_dialog, container, false)
+
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView, adapter: AttendanceAdapter) {
@@ -149,14 +88,19 @@ class CalendarDialogFragment : DialogFragment() {
         dialog.setContentView(customDialogView)
 
         // 다이얼로그의 크기를 화면 전체로 설정
-        val width = ViewGroup.LayoutParams.MATCH_PARENT
-        val height = (ScreenUtils.getScreenSize(requireContext()).y * 0.8).toInt()
+        val width = (ScreenUtils.getScreenSize(requireContext()).x * 0.9).toInt()
+        //  ViewGroup.LayoutParams.MATCH_PARENT
+        val height = (ScreenUtils.getScreenSize(requireContext()).y * 0.75).toInt()
+        // ViewGroup.LayoutParams.MATCH_PARENT
         dialog.window?.setLayout(width, height)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
 
         // WindowManager.LayoutParams를 사용하여 상단 마진 추가
         val layoutParams = dialog.window?.attributes
         layoutParams?.y = - (height * 0.02).toInt() // 상단 마진 값 (원하는 값으로 조절)
+
         dialog.window?.attributes = layoutParams
+        dialog.setCancelable(true)
 
         return dialog
     }
@@ -173,38 +117,4 @@ class CalendarDialogFragment : DialogFragment() {
         }
     }
 
-    class MyItemTouchHelperCallback(private val adapter: AttendanceAdapter) : ItemTouchHelper.Callback() {
-
-        override fun getMovementFlags(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder
-        ): Int {
-            Log.d("DialogFragment", "getMovementFlag: $recyclerView")
-            val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
-            return makeMovementFlags(dragFlags, 0)
-        }
-
-        override fun onMove(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder,
-            target: RecyclerView.ViewHolder
-        ): Boolean {
-            // 아이템 위치 변경 처리
-            Log.d("DialogFragment", "onMove: $recyclerView")
-//            if (recyclerView == adapter1.recyclerView) {
-//                adapter1.onItemMove(viewHolder.bindingAdapterPosition, target.bindingAdapterPosition)
-//            } else if (recyclerView == adapter2.recyclerView) {
-//                adapter1.onItemMove(viewHolder.bindingAdapterPosition, target.bindingAdapterPosition)
-//            }
-
-            return adapter.onItemMove(viewHolder.bindingAdapterPosition, target.bindingAdapterPosition)
-
-//            return true
-        }
-
-        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-            // 드래그 후 손을 뗄 때 호출됨
-            Log.d("DialogFragment", "onSwiped: $direction")
-        }
-    }
 }
